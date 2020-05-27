176791b25c27f22c0c4415ee1b764217634f47e5
Alex Hill <alexander.d.hill.89@gmail.com>
2020-05-26 17:09:56 -0700

    Check the TOC is up-to-date for PRs
    A Github action will run on pushes, regenerating the TOC and complaining
    if there are differences with the version from the checked-out tree.
    The generator no longer uses `os.walk` because dirctory traversal order
    is not guaranteed, so we implement the traversal ourselves to guarantee
    the TOC will be generated with elements in a predictable order.

--- index_gen.py
+++ index_gen.py
@@ -3,27 +3,42 @@
 import os
 
 seperator = "  "
-return_strings = []
 
 # Based on https://stackoverflow.com/a/16974952
 # Thank you based Ajay && based zaooza
 
-for root, dirs, files in os.walk(u"."):
-    path = root.split(os.sep)
+def walk_lexicographic(path='.'):
+    '''
+    Traverses a directory tree. Each level is traversed in lexicographical
+    order, and when a subdirectory is encountered it will be yielded
+    immediately before being descended into, recursively, and then traversal of
+    the original directory is continued. Entries beginning with a dot are never
+    yielded, nor descended into.
+    '''
 
-    if ".git" in path:
-        continue
+    entries = sorted(
+        (entry for entry in os.scandir(path) if entry.name[0] != '.'),
+        key=lambda e: e.name
+    )
+    for entry in entries:
+        yield entry
+        if entry.is_dir():
+            yield from walk_lexicographic(entry.path)
 
-    if path != ["."]:
-        return_strings.append((len(path) - 2) * seperator + "* " + os.path.basename(root))
-    for file in files:
-        if file not in ["README.md", "index.md", "index_gen.py"]:
-            link = "/".join([root, file])
-            prefix = (len(path) -1) * seperator + "* "
-            file_nice_name = file.split(".")[0]
-            return_strings.append(f"{prefix}[{file_nice_name}]({link})")
+def toc_lines(path='.'):
+    '''
+    Yield Markdown for a table of contents for the files rooted at ``path'',
+    line by line.
+    '''
+    for entry in walk_lexicographic(path):
+        if entry.is_dir():
+            yield (entry.path.count('/') - 1) * seperator + "* " + entry.name
+        elif entry.name not in ('README.md', 'index.md', 'index_gen.py'):
+            link = entry.path
+            prefix = (entry.path.count('/') - 1) * seperator + "* "
+            stem = entry.name.split('.')[0]
+            yield f'{prefix}[{stem}]({link})'
 
 with open("index.md", "w") as index_file:
-    return_strings = map(lambda x: x + '\n', return_strings)
-    index_file.writelines(return_strings)
-
+    for toc_line in toc_lines():
+        print(toc_line, file=index_file)
